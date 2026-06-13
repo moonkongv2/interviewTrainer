8d34579e921a8245fdc23ea8d7f2e6b46de1cb49
moonkong <moonskong@gmail.com>
2026-06-13 22:52:58 +0900

    initial commit

---
+++ interviewTrainer/interviewTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  interviewTrainerApp.swift
+//  interviewTrainer
+//
+//  Created by Moonsik Kong on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct interviewTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ interviewTrainerTests/interviewTrainerTests.swift
@@ -0,0 +1,17 @@
+//
+//  interviewTrainerTests.swift
+//  interviewTrainerTests
+//
+//  Created by Moonsik Kong on 6/13/26.
+//
+
+import Testing
+@testable import interviewTrainer
+
+struct interviewTrainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
