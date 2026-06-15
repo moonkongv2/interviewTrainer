008a34a5818681d7d15ced4ab5c9a874893e3869
moonkong <moonskong@gmail.com>
2026-06-15 12:22:40 +0900

    Add startup splash screen

--- interviewTrainer/interviewTrainerApp.swift
+++ interviewTrainer/interviewTrainerApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 @main
 struct interviewTrainerApp: App {
@@ -13,8 +14,58 @@ struct interviewTrainerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            SplashRootView()
                 .environmentObject(progressStore)
+        }
+    }
+}
+
+private struct SplashRootView: View {
+    @State private var isShowingSplash = true
+
+    var body: some View {
+        ZStack {
+            HomeView()
+                .opacity(isShowingSplash ? 0 : 1)
+
+            if isShowingSplash {
+                SplashImageView()
+                    .transition(.opacity)
+            }
+        }
+        .task {
+            try? await Task.sleep(nanoseconds: 3_000_000_000)
+
+            withAnimation(.easeInOut(duration: 0.35)) {
+                isShowingSplash = false
+            }
+        }
+    }
+}
+
+private struct SplashImageView: View {
+    private var splashImage: UIImage? {
+        guard let url = Bundle.main.url(forResource: "splash_image", withExtension: "png") else {
+            return nil
+        }
+
+        return UIImage(contentsOfFile: url.path)
+    }
+
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+                Color(.systemBackground)
+
+                if let splashImage {
+                    Image(uiImage: splashImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: proxy.size.width, height: proxy.size.height)
+                        .clipped()
+                }
+            }
+            .ignoresSafeArea()
         }
     }
 }
